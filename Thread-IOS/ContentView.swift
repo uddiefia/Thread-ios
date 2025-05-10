aaf350b19b6243be3fd036e2050200c18830b1de
Udara Damitha <udaradamitha@gmail.com>
2025-05-10 21:47:53 +0530

    Initial Commit

---
+++ Thread-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thread-IOS
+//
+//  Created by udara on 2025-05-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thread-IOS/Thread_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_IOSApp.swift
+//  Thread-IOS
+//
+//  Created by udara on 2025-05-10.
+//
+
+import SwiftUI
+
+@main
+struct Thread_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
