718c0cf6d367a661da198235804d5fc03da1a0ea
Udara Damitha <udaradamitha@gmail.com>
2025-05-11 23:27:11 +0530

    FeedView with ThreadCells

--- Thread-IOS/Core/Feed/View/FeedView.swift
+++ Thread-IOS/Core/Feed/View/FeedView.swift
@@ -5,7 +5,9 @@ struct FeedView: View {
         NavigationStack{
             ScrollView(showsIndicators: false){
                 LazyVStack{
-                    
+                    ForEach(0 ... 10 ,id:\.self){ thread in
+                        ThreadCell()
+                    }
                 }
             }
         }
