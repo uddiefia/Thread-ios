003f0f75bcce18a3dcb5363e47f7f787bde2967d
Udara Damitha <udaradamitha@gmail.com>
2025-05-12 19:40:45 +0530

    User Bio and Stats

--- Thread-IOS/Core/Profile/View/ProfileView.swift
+++ Thread-IOS/Core/Profile/View/ProfileView.swift
@@ -2,7 +2,31 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("ProfileView")
+        ScrollView{
+//           bio and stats
+            HStack(alignment:.top) {
+                VStack(alignment: .leading, spacing: 12) {
+                    //                fullname & username
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("Udarapriya Damitha")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        
+                        Text("udara_damith")
+                            .font(.subheadline)
+                    }
+                    
+                    Text("full stack mobile developer")
+                        .font(.footnote)
+                    
+                    Text("2 followers")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                CircularProfileImageView()
+            }.padding(.horizontal)
+        }
     }
 }
 
